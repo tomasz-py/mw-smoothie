49c7a611130505f4eae9d35f57e2cc426a7ffa70
Tomasz-py <tomasz.pyclik@gmail.com>
2019-06-10 17:30:38 +0200

    add to top arrow

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-console.log("script");
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("upArrow").style.display = "block";
+  } else {
+    document.getElementById("upArrow").style.display = "none";
+  }
+}
+
+function backToTop() {
+  let scrollDuration = 300;
+
+  console.log(-window.scrollY);
+  let scrollStep = -window.scrollY / (scrollDuration / 15),
+    scrollInterval = setInterval(function() {
+      if (window.scrollY != 0) {
+        window.scrollBy(0, scrollStep);
+      } else clearInterval(scrollInterval);
+    }, 15);
+}
